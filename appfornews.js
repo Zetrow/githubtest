ca50481f817d10ad72100139e537c66c11d316b4
YhuZzi <98905694+YhuZzi@users.noreply.github.com>
2022-06-13 16:17:11 +0200

    Add files via upload

---
+++ appfornews.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click', function() {
+    document.querySelector('.contenue').style.display = 'block';
+})
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.querySelector('.contenue').style.display = 'none';
+})
