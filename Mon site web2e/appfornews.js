298b9d24c9e9c65e0c4d8ac0e2a3ce94e65df724
Zetrow <mathias.pinto@epita.fr>
2023-03-30 11:22:56 -0400

    pUSH
    S

--- Mon site web2e/appfornews.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button').addEventListener('click', function() {
-    document.querySelector('.contenue').style.display = 'block';
-})
-
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.querySelector('.contenue').style.display = 'none';
-})
